252ced01ab2731597f99107f83ac78b284aa09fb
Arne Magnus Sund <arnem.sund@gmail.com>
2017-10-02 12:04:51 +0200

    Keras testing, setting up model

--- keras_test.py
+++ keras_test.py
@@ -1,17 +1,17 @@
-from keras.models import Sequencial
-from keras.layers import Dense, Activation, Noise, Dropout
+from keras.models import Sequential
+from keras.layers import Dense, Activation, BatchNormalization, GaussianNoise, GaussianDropout
 from random_data import generateData
 
 
 
-model = Sequencial()
+model = Sequential()
 model.add(Dense(32, input_shape=(200,)))
-model.add(Dense(7, input_shape=(32,),activation='softmax')
-#model.add(GaussianNoise(stddev))
-#model.add(GaussianDropout(0.5))
-
-#keras.layers.noise.GaussianNoise(stddev)
-#keras.layers.noise.GaussianDropout(0.5)
+model.add(Dense(7, input_shape=(32,),activation='softmax'))
+model.add(BatchNormalization())
+model = GaussianNoise(0.5)(model)
+model = GaussianDropout(0.5)(model)
+model.add(Dense(32, input_shape=(7,),activation='softmax'))
+model.add(Dense(200, input_shape=(32,)))
 
 
 model.compile()
