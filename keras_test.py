74cbdb5a9be3d52015ebbfe0833eb8e34fdee4bf
Arne Magnus Sund <arnem.sund@gmail.com>
2017-10-02 12:27:02 +0200

    Commenting on code

--- keras_test.py
+++ keras_test.py
@@ -3,13 +3,18 @@ from keras.layers import Dense, Activation, BatchNormalization, GaussianNoise, G
 from random_data import generateData
 
 
-
+#Sequential model
 model = Sequential()
+
+#Densing ("Transmitter")
 model.add(Dense(32, input_shape=(200,)))
 model.add(Dense(7, input_shape=(32,),activation='softmax'))
+#Normalization
 model.add(BatchNormalization())
+#Adding noise (channel simulation)
 model = GaussianNoise(0.5)(model)
 model = GaussianDropout(0.5)(model)
+#Densing ("Receiver")
 model.add(Dense(32, input_shape=(7,),activation='softmax'))
 model.add(Dense(200, input_shape=(32,)))
 
